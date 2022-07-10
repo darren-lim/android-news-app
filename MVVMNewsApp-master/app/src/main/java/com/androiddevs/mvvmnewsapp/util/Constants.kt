59bd88c376aaa4e7c791246148617f3ba0442c71
darren <limdarrenj@gmail.com>
2022-07-09 17:59:50 -0700

    retrofit

---
+++ MVVMNewsApp-master/app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "813f33e2e6c847dbabba590006f8855f"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
